9ee6af51b93e59d86fd8105d8ab07563e5ff432a
vedantnaik3 <naikvedant22705@gmail.com>
2025-07-25 12:44:00 +0530

    inverted rotated half pyramid code

---
+++ advpattern/inhalfpyramid.java
@@ -0,0 +1,13 @@
+public class inhalfpyramid {
+    public static void inverted_rotated_half_pyramid(int n){
+        for(int i = 1; i <= n; i++){
+            for (int j = 1; j<= n-i+1;j++){
+                System.out.print(j + " ");
+            }
+            System.out.println();
+        }
+    }
+    public static void main(String[] args) {
+        inverted_rotated_half_pyramid(5);
+    }
+}
