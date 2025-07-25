cf950e452cc382ccecc6087e093bac7f63631ca0
vedantnaik3 <naikvedant22705@gmail.com>
2025-07-25 12:41:27 +0530

    flyod number code

---
+++ advpattern/flyoydstri.java
@@ -0,0 +1,17 @@
+public class flyoydstri {
+    public static void floyds_triangle(int n){
+        //outer loop
+        int counter =1;
+        for ( int i = 1;i <= n;i++){
+            // inner - how many times will counter be printed
+            for (int j = 1;j<= i;j++) {
+                System.out.print(counter+" ");
+                counter++;
+            }
+            System.out.println();
+        }
+    }
+    public static void main(String[] args) {
+        floyds_triangle(5);
+    }
+}
